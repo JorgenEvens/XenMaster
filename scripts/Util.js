6304b01fd1bc96e18888034a14d95f6f204f0837
Jorgen Evens <jorgen.evens@gmail.com>
2011-10-02 02:32:50 +0200

    Merged a detached head into master

---
+++ scripts/Util.js
@@ -0,0 +1,15 @@
+(function(){
+	
+	
+	
+	Util = function(){};
+	
+	Util.isArray = function( value ) {
+		return Object.prototype.toString.call(value) === "[object Array]";
+	};
+	
+	Util.argumentsToArray = function( value ) {
+		return Array.prototype.slice.call( value );
+	}
+	
+}());
