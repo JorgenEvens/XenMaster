515c1522b3f7f46379d36a231e8368acf9d6fb35
Jorgen Evens <jorgen.evens@gmail.com>
2011-11-29 22:38:16 +0100

    Detach and Remove functionality on SR

--- view/storage/list/code.js
+++ view/storage/list/code.js
@@ -1,10 +1,44 @@
 (function( $, app ){
-	var dom = $(this.dom);
+	var tpl = this,
+		dom = $(tpl.dom);
+	
+	tpl.capture( 'click' );
+	
+	tpl.on( 'sr_new', function( e ){
+		console.log( arguments );
+	});
+	
+	
 	
 	app.load( 'js://api/sr', function( SR ) {
 		
 		var base = dom.find('.repo header ul').clone();
-		base.find('li').html('');
+		
+		tpl.on( 'sr_remove', function( e ) {
+			dom.find( '.repo input:checked' ).each(function(){
+				var ref = $(this).val();
+				
+				if( ref.length > 0 ) {
+					(new SR(ref)).destroy(function( result ){
+						console.log( result );
+					});
+				}
+			});
+		});
+		
+		tpl.on( 'sr_detach', function( e ) {
+			dom.find( '.repo input:checked' ).each(function(){
+				var ref = $(this).val();
+				
+				if( ref.length > 0 ) {
+					(new SR(ref)).destroy(function( result ){
+						console.log( result );
+					});
+				}
+			});
+		});
+		
+		base.find('li').filter(':not(.selection)').html('');
 		
 		SR.getAll( function( result ) {
 			for( item in result ) {
@@ -13,9 +47,10 @@
 				var row = base.clone(),
 					fields = row.find('li');
 				
-				$(fields[0]).text( item.name );
-				$(fields[1]).text( item.otherConfig.location );
-				$(fields[2]).text( item.type );
+				fields.filter('.selection input').val( item.reference );
+				fields.filter('.name').text( item.name );
+				fields.filter('.location').text( item.otherConfig.location );
+				fields.filter('.type').text( item.type );
 				
 				dom.find('.repo').append( row );
 			}
