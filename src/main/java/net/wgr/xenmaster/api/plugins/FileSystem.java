8f0b02dcc2d197016db48ce2bd4a5d9648ff0b2d
Double-U <wannes321@gmail.com>
2011-12-12 23:24:17 +0100

    Added functionality - Added SR.getPBDs and SR.getVDIs - Added method SR.getDiskStructure to plugin API to expose Host disk structure - Changed output of FileSystem.getDiskDevices

--- src/main/java/net/wgr/xenmaster/api/plugins/FileSystem.java
+++ src/main/java/net/wgr/xenmaster/api/plugins/FileSystem.java
@@ -7,7 +7,12 @@
 package net.wgr.xenmaster.api.plugins;
 
 import com.google.gson.Gson;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import net.wgr.xenmaster.api.Host;
 import net.wgr.xenmaster.controller.BadAPICallException;
 
@@ -19,10 +24,34 @@ import net.wgr.xenmaster.controller.BadAPICallException;
 public class FileSystem {
     protected final static String FILESYTEM_PLUGIN = "xm-filesystem";
     protected static Gson gson = new Gson();
+    protected final static Pattern PARTITIION_PATTERN = Pattern.compile("([A-z]{3})(\\d+)");
     
     public static String[] getDiskDevices(Host host) throws BadAPICallException {
         String result = host.callPlugin(FILESYTEM_PLUGIN, "disks", new HashMap<String, String>());
         String[] str = gson.fromJson(result, String[].class);
-        return str;
+        ArrayList<String> disks = new ArrayList<>();
+        
+        for (String device : str) {
+            if (!PARTITIION_PATTERN.matcher(device).matches()) disks.add(device);
+        }
+        return disks.toArray(new String[0]);
+    }
+    
+    public static Map<String, ArrayList<String>> getDiskStructure(Host host) throws BadAPICallException {
+        HashMap<String, ArrayList<String>> struct = new HashMap<>();
+        String result = host.callPlugin(FILESYTEM_PLUGIN, "disks", new HashMap<String, String>());
+        String[] str = gson.fromJson(result, String[].class);
+        
+        for (String device : str) {
+            Matcher m = PARTITIION_PATTERN.matcher(device);
+            if (m.matches()) {
+                if (!struct.containsKey(m.group(1))) struct.put(m.group(1), new ArrayList<String>());
+                struct.get(m.group(1)).add(device);
+            } else if (!device.startsWith("sd")) {
+                struct.put(device, null);
+            }
+        }
+        
+        return struct;
     }
 }
