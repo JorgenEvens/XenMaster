e107ca7b68a290f1c2c3669e4e5eb7fe38165b52
Double-U <wannes321@gmail.com>
2012-01-05 02:12:15 +0100

    Made LogEntry more flexible + emit XAPI events - Emitter listens to XAPI events, logs the difference between entities and emits them to the clients - LogEntry now has a generic subject field which may contain anything related to the LogEntry

--- src/main/java/net/wgr/xenmaster/monitoring/Emitter.java
+++ src/main/java/net/wgr/xenmaster/monitoring/Emitter.java
@@ -6,9 +6,13 @@
  */
 package net.wgr.xenmaster.monitoring;
 
+import java.util.Map;
+import net.wgr.core.ReflectionUtils;
 import net.wgr.wcp.Commander;
 import net.wgr.wcp.Scope;
 import net.wgr.wcp.command.Command;
+import net.wgr.xenmaster.api.Event;
+import net.wgr.xenmaster.api.util.CachingFacility;
 
 /**
  * Issues requested monitoring data "formally and with authority"
@@ -16,7 +20,27 @@ import net.wgr.wcp.command.Command;
  * @author double-u
  */
 public class Emitter {
+    
+    public void Emitter() {
+    }
+    
+    public void listenToEvents(EventHandler eh) {
+        eh.addListener(new EventHandler.EventListener() {
+            
+            @Override
+            public void eventOcurred(Event event) {
+                Map<String, Object> diff = ReflectionUtils.diff(CachingFacility.get(event.getSnapshot().getReference(), event.getSnapshot().getClass()), event.getSnapshot());
+                if (diff.size() < 1) {
+                    // Nothing was changed?
+                    return;
+                }
+                LogEntry le = new LogEntry(event.getReference(), event.getEventClass(), event.getOperation(), diff, event.getTimestamp(), LogEntry.Level.INFORMATION);
+                emit(le);
+            }
+        });
+    }
+    
     public static void emit(LogEntry le) {
-        Commander.getInstance().commandeer(new Command("log", "write", le), new Scope(Scope.Target.ALL));
+        Commander.getInstance().commandeer(new Command("log", "event", le), new Scope(Scope.Target.ALL));
     }
 }
