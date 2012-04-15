80da2c663f2f90a2d60d13b2c4ffd7b201fd532c
Double-U <wannes321@gmail.com>
2012-04-15 13:28:36 +0200

    Reverted to normal echo in post-install - echo -c doesn't do anything (useful)

--- store/setup/post-install.sh
+++ store/setup/post-install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-echo -c 'Running XenMaster post-install script...';
-echo -c 'Making xen boot by default';
+echo 'Running XenMaster post-install script...';
+echo 'Making xen boot by default';
 
 sed -i 's/GRUB_DEFAULT=.\+/GRUB_DEFAULT="Xen 4.1-amd64"/' /etc/default/grub;
 update-grub;
 
-echo -c 'Installing empty interfaces config';
+echo 'Setting up networking...';
 
 cat > /etc/network/interfaces <<EOF 
 # This file describes the network interfaces available on your system
@@ -18,7 +18,7 @@ iface lo inet loopback
 
 EOF
 
-echo -c 'Setting up the xensource-inventory file';
+echo 'Setting up the xensource-inventory file';
 
 control_domain=`uuidgen`;
 installation=`uuidgen`;
@@ -30,69 +30,50 @@ CONTROL_DOMAIN_UUID='${control_domain}'
 INSTALLATION_UUID='${installation}'
 MANAGEMENT_INTERFACE='xenbr0'
 PRIMARY_DISK='/dev/sda1'
-PRODUCT_VERSION=6.0.0
 EOF
 
-echo -c 'Setting xapi as the toolstack';
+echo 'Setting xapi as the toolstack';
 cat > /etc/default/xen << EOF
 TOOLSTACK=xapi
 EOF
 
-echo -c 'Make sure xend does *not* start';
+echo 'Make sure xend does *not* start';
 sed -i -e 's/xend_start$/#xend_start/' -e 's/xend_stop$/#xend_stop/' /etc/init.d/xend;
 update-rc.d xendomains disable;
 
-echo -c 'Make SSL comms start on boot';
+echo 'Make SSL comms start on boot';
 
 update-rc.d xapissl defaults;
 
-echo -c 'Installing XAPI plugins';
+echo 'Installing XAPI plugins';
 
 wget http://#{bootstrap-server-address}/setup/plugins.tar.gz -O /root/xapi-plugins.tar.gz;
 tar -C /usr/lib/xcp/plugins -xvzf /root/xapi-plugins.tar.gz;
 
-echo -c 'Saying hi to bootstrap server';
+echo 'Saying hi to bootstrap server';
 
 mv /etc/motd.tail /etc/motd.tail.default;
 wget http://#{bootstrap-server-address}/setup/motd -O /etc/motd.tail
 
-echo -c 'Linking keymap files';
+echo 'Linking keymap files';
 mkdir -p /usr/share/qemu/
 ln -s /usr/share/qemu-linaro/keymaps /usr/share/qemu/keymaps
 
 cat > /root/setup-done.sh << EOF
 #!/bin/sh
-echo -c 'Removing message...';
+echo 'Removing message...';
 mv /etc/motd.tail.default /etc/motd.tail;
-echo -c 'Disabling rc.local...';
-chmod -x /etc/rc.local
-echo -c 'Done! Enjoy your coffee';
+echo 'Done! Enjoy your coffee';
 EOF
-
-cat > /etc/rc.local << EOF
-echo -c 'Set all interfaces to DHCP configuration in XAPI';
-python << EOG
-import XenAPI
-session = XenAPI.xapi_local()
-session.login_with_password("root", "")
-pifs = session.xenapi.PIF.get_all()
-for pif in pifs:
-session.xenapi.PIF.reconfigure_ip(pif,"dhcp","","","","")
-EOG
-
-exit 0
-EOF
-
-chmod +x /etc/rc.local
 
 cat > /etc/xen/scripts/qemu-ifup << EOF
 #!/bin/sh
 
-echo -c 'config qemu network with xen bridge for '
+echo 'config qemu network with xen bridge for '
 echo \$*
 
 ifconfig \$1 0.0.0.0 up
 ovs-vsctl add-port \$2 \$1
 EOF
 
-echo -c 'Post Install finished';
+echo 'Post Install finished';
