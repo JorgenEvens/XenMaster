449d0b32c4210f969f6de16cd110d550840c0eb1
Double-U <wannes321@gmail.com>
2013-03-15 19:30:38 +0100

    Make sure Event handling does not block

--- src/main/java/org/xenmaster/monitoring/EventHandler.java
+++ src/main/java/org/xenmaster/monitoring/EventHandler.java
@@ -19,75 +19,85 @@ package org.xenmaster.monitoring;
 
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
+import net.wgr.utility.GlobalExecutorService;
 
 import org.apache.log4j.Logger;
 import org.xenmaster.api.entity.Event;
 import org.xenmaster.controller.BadAPICallException;
 
 /**
- * 
+ *
  * @created Jan 4, 2012
  * @author double-u
  */
 public class EventHandler implements Runnable {
-    
+
     protected boolean run;
     protected Thread thread;
     protected List<EventListener> listeners;
-    
+
     public EventHandler() {
         this.listeners = new CopyOnWriteArrayList<>();
         this.thread = new Thread(this);
         this.thread.setName("EventHandler");
     }
-    
+
     public void addListener(EventListener el) {
         this.listeners.add(el);
     }
-    
+
     public void addListener(EventListener el, int index) {
         this.listeners.add(index, el);
     }
-    
+
     public void stopListener(EventListener el) {
         this.listeners.remove(el);
     }
-    
+
     public void start() {
         run = true;
         thread.start();
     }
-    
+
     public void stop() {
         run = false;
     }
-    
+
     @Override
     public void run() {
         try {
-            // todo this should always take place on pool masters, explicitly set context
+            // TODO this should always take place on pool masters, explicitly set context
             Event.register();
         } catch (BadAPICallException ex) {
             Logger.getLogger(getClass()).error("Failed to register to events", ex);
         }
-        
+
         while (run) {
             try {
-                List<Event> latest = Event.nextEvents();
-                for (Event event : latest) {
-                    for (EventListener el : listeners) {
-                        el.eventOcurred(event);
+                final List<Event> latest = Event.nextEvents();
+                
+                // We cannot wait on the event listeners
+                // TODO: this is very crufty
+                GlobalExecutorService.get().submit(new Runnable() {
+                    @Override
+                    public void run() {
+                        for (Event event : latest) {
+                            for (EventListener el : listeners) {
+                                el.eventOcurred(event);
+                            }
+                        }
                     }
-                }
+                });
+
             } catch (BadAPICallException ex) {
                 run = false;
                 Logger.getLogger(getClass()).error("Failed to retrieve latest events", ex);
             }
         }
     }
-    
+
     public static interface EventListener {
-        
+
         public void eventOcurred(Event event);
     }
 }
